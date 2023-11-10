f2d945d19095668691b5a730f4f0a15c75cdfdf1
KimDabomi <cdabomi@nate.com>
2023-11-11 03:31:20 +0900

    feat: Add ChristmasController, Application methods

---
+++ src/main/java/christmas/controller/ChristmasController.java
@@ -0,0 +1,46 @@
+package christmas.controller;
+
+import christmas.domain.order.Order;
+import christmas.view.InputView;
+import christmas.view.OutputView;
+
+public class ChristmasController {
+    private final InputView inputView;
+    private final OutputView outputView;
+
+    public ChristmasController(InputView inputView, OutputView outputView) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+    }
+
+    public void start() {
+        startDate();
+    }
+
+    private void startDate() {
+        while (true) {
+            try {
+                String dateInput = inputView.readDate();
+                int date = Order.getDate(dateInput);
+
+                startOrder();
+
+                outputView.showEventPreview(date);
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+    private void startOrder() {
+        while (true) {
+            try {
+                Order order = inputView.readMenuOrder();
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+}
