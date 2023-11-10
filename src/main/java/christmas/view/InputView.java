965c2b05a4047f6d439d4cb92b455d30a8dbf34b
KimDabomi <cdabomi@nate.com>
2023-11-11 03:29:04 +0900

    feat: Add InputView, OutputView

---
+++ src/main/java/christmas/view/InputView.java
@@ -0,0 +1,27 @@
+package christmas.view;
+
+import camp.nextstep.edu.missionutils.Console;
+import christmas.domain.order.OrderItem;
+import christmas.domain.order.Order;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class InputView {
+    public InputView() {
+    }
+
+    public String readDate() {
+        System.out.println("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        return Console.readLine();
+    }
+
+    public Order readMenuOrder() {
+        System.out.println("주문하실 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        String[] menuItems = Console.readLine().split(",");
+        List<OrderItem> orderItemList = new ArrayList<>();
+
+        return  Order.getOrderItems(menuItems, orderItemList);
+    }
+}
---
+++ src/main/java/christmas/view/OutputView.java
@@ -0,0 +1,10 @@
+package christmas.view;
+
+public class OutputView {
+    public OutputView() {
+    }
+
+    public void showEventPreview(int date) {
+        System.out.println("12월 " + date + "일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!");
+    }
+}
